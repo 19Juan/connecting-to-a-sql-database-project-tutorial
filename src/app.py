ff1d254a85486a58e2cdcf895aff88e4c14764b8
19Juan <19juan.cortes@gmail.com>
2025-01-19 14:14:05 +0000

    .

--- src/app.py
+++ src/app.py
@@ -3,14 +3,24 @@ from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
 
-# load the .env file variables
+
 load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
 
-connection_string = f"postgresql://{os.getenv('gitpod')}:{os.getenv('postgres')}@{os.getenv('localhost')}/{os.getenv('sample_db')}"
-engine = create_engine(connection_string).execution_options(autocommit=True)
-engine.connect()
+#connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+#engine = create_engine(connection_string).execution_options(autocommit=True)
+#engine.connect()
+
+def connect():
+    global engine 
+    connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    print("Starting the connection...")
+    engine = create_engine(connection_string).execution_options(autocommit=True)
+    engine.connect()
+    return engine
+
+connect()
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 engine.execute(""" CREATE TABLE publishers(
